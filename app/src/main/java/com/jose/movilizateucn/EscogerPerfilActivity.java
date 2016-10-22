d5c7228766526ec3125d2e86f7adab38a70374ba
JoseEsp <jes004@alumnos.ucn.cl>
2016-10-22 18:30:54 -0300

    Initial commit

---
+++ app/src/main/java/com/jose/movilizateucn/EscogerPerfilActivity.java
@@ -0,0 +1,13 @@
+package com.jose.movilizateucn;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class EscogerPerfilActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_escoger_perfil);
+    }
+}
---
+++ app/src/main/java/com/jose/movilizateucn/MainActivity.java
@@ -0,0 +1,30 @@
+package com.jose.movilizateucn;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        configureBtnLogin();
+    }
+
+    private void configureBtnLogin(){
+        final Intent myIntent = new Intent(this, EscogerPerfilActivity.class);
+        Button btnLogin = (Button) findViewById(R.id.btnLogin);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(myIntent);
+            }
+        });
+    }
+
+
+}
