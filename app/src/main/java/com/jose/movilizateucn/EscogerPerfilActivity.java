5cdca342ef0fe2b988da8d308486d2b7825bd483
JoseEsp <jes004@alumnos.ucn.cl>
2016-10-23 12:19:46 -0300

    Commit

--- app/src/main/java/com/jose/movilizateucn/EscogerPerfilActivity.java
+++ app/src/main/java/com/jose/movilizateucn/EscogerPerfilActivity.java
@@ -1,7 +1,10 @@
 package com.jose.movilizateucn;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class EscogerPerfilActivity extends AppCompatActivity {
 
@@ -9,5 +12,28 @@ public class EscogerPerfilActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_escoger_perfil);
+        configureButtons();
+    }
+
+    private void configureButtons(){
+        Button btnChofer = (Button) findViewById(R.id.btnChofer);
+        Button btnPasajero = (Button) findViewById(R.id.btnPasajero);
+
+        final Intent choferActivity = new Intent(this, ChoferActivity.class);
+        final Intent pasajeroActivity = new Intent(this, PasajeroActivity.class);
+
+        btnChofer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(choferActivity);
+            }
+        });
+
+        btnPasajero.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(pasajeroActivity);
+            }
+        });
     }
 }
