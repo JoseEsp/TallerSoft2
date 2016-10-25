2bc509ff709ae6f90375f812a418ca71379f8eb5
JoseEsp <jes004@alumnos.ucn.cl>
2016-10-25 17:05:19 -0300

    Commit

--- app/src/main/java/com/jose/movilizateucn/ChoferActivity.java
+++ app/src/main/java/com/jose/movilizateucn/ChoferActivity.java
@@ -3,6 +3,9 @@ package com.jose.movilizateucn;
 import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Spannable;
+import android.text.SpannableString;
+import android.text.style.ForegroundColorSpan;
 import android.view.View;
 import android.widget.Button;
 import android.widget.RatingBar;
@@ -18,25 +21,33 @@ public class ChoferActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chofer);
         configureStarBar();
-        updateScore();
-        test(); //método para probar solamente (modifico el starBar y el lblScore
     }
 
     private void configureStarBar(){
         starBar = (RatingBar) findViewById(R.id.ratingBarChofer);
         lblScore = (TextView) findViewById(R.id.lblStarScoreChofer);
-        lblScore.setTextColor(Color.BLACK);
+        updateScore();
+        test(); //método para probar solamente (modifico el starBar y el lblScore
     }
 
     private void updateScore(){
         float actualScore = starBar.getRating();
-        if (actualScore < 2f){
-            lblScore.setTextColor(Color.RED);
-        }else if (actualScore >= 2f && actualScore < 4.0f){
-            lblScore.setTextColor(Color.BLACK);
+        String strScore = String.format("%.1f/%d", actualScore, starBar.getNumStars());
+        Spannable colorText = new SpannableString(strScore);
+        ForegroundColorSpan color = null;
+        if (actualScore < 2.5f){
+            color = new ForegroundColorSpan(Color.RED);
+        }else if (actualScore >= 2.5f && actualScore < 4f){
+            color = new ForegroundColorSpan(Color.BLACK);
         }else{
-            lblScore.setTextColor(Color.BLUE);
+            color = new ForegroundColorSpan(Color.BLUE);
         }
+        //Colorea antes del '/'
+        colorText.setSpan(color, 0, strScore.indexOf("/"), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+        //Colorea después del '/'
+        colorText.setSpan(new ForegroundColorSpan(Color.BLACK), strScore.indexOf("/"),
+                          strScore.length(), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+        lblScore.setText(colorText);
     }
 
     private void test(){
@@ -45,8 +56,6 @@ public class ChoferActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 float actualScore = (float) (Math.random() * (float) starBar.getNumStars());
-                String strScore = String.format("%.1f/%d", actualScore, starBar.getNumStars());
-                lblScore.setText(strScore);
                 starBar.setRating(actualScore);
                 updateScore();
             }
