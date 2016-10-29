7d21192669271c9b01dfef972b84284e7bec7ef7
JoseEsp <jes004@alumnos.ucn.cl>
2016-10-29 18:45:09 -0300

    Agregado Activity_Registrarse, y unos cambios (los land) a unos activitys anteriores.

--- app/src/main/java/com/jose/movilizateucn/MainActivity.java
+++ app/src/main/java/com/jose/movilizateucn/MainActivity.java
@@ -17,12 +17,23 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void configureBtnLogin(){
-        final Intent myIntent = new Intent(this, EscogerPerfilActivity.class);
+        final Intent perfilActivity = new Intent(this, EscogerPerfilActivity.class);
         Button btnLogin = (Button) findViewById(R.id.btnLogin);
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(myIntent);
+                startActivity(perfilActivity);
+            }
+        });
+    }
+
+    private void configureBtnRegistrar(){
+        final Intent registrarseActivity = new Intent(this, RegistrarseActivity.class);
+        Button btnRegistrar = (Button) findViewById(R.id.btnRegistrar);
+        btnRegistrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(registrarseActivity);
             }
         });
     }
