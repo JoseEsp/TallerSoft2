3229e2f05bc4719328568a42d22ee7297578860f
JoseEsp <jes004@alumnos.ucn.cl>
2016-11-29 15:22:48 -0300

    29-11-2016

--- app/src/main/java/com/jose/movilizateucn/GenerarSolicitudMap.java
+++ app/src/main/java/com/jose/movilizateucn/GenerarSolicitudMap.java
@@ -9,7 +9,6 @@ import android.location.LocationManager;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.ProgressBar;
